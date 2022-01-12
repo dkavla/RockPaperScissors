0a7bac0f4ffa5f7f24dd052328052693dab4b9af
David <dkowalczyk135@gmail.com>
2022-01-12 16:11:49 -0500

    Create computerPlay function

---
+++ javascript/app.js
@@ -0,0 +1,16 @@
+
+// A function that returns the computers choice
+let computerPlay = () => {
+    let choices = ['rock', 'paper', 'scissors'];
+
+    let pick = Math.floor(Math.random() * choices.length);
+    console.log(choices[pick]);
+};
+
+
+
+
+
+
+
+
